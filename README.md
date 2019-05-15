90571ce3e7b689b4a4591a65bc72bc271c8f1070
scarlettperry <si.perry15@gmail.com>
2019-05-14 23:15:45 -0400

    deleted some boilerplate files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# the-office-family
+# react-app-notes
+
+- create a react app, run `npx create-react-app my-app`
+    - make sure have latest version of node
+        -`nvm install 10.7.0` and then `nvm use 10.7.0`
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+//filter component - filter employees by branch and/or department
+//search bar - type in employee name
+//all employee componet & then single employee component
+//create employee - form to add a new employee
+//edit employee - form to edit a employee
+
+export default App;
